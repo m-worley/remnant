fbd0f268dcef87d6a35b7744efe5c3a24a8db0c8
Megan Worley <megan.worley@live.com>
2016-03-07 21:50:55 -0800

    Updated build and project structure.
    Added .gitignore.  Updated build to assemble and include correct files
    to reflect project structure.  Updated source to be HelloWorld as a
    basic starting point.

--- build.bat
+++ build.bat
@@ -3,23 +3,25 @@
 ::Create binary directory
 set SRC_DIR="%~dp0src"
 set BUILD_DIR="%~dp0bin"
+set INCLUDES=%~dp0include\
+set EXTERNAL_INCLUDES=%~dp0external\include\
 mkdir %BUILD_DIR%
 
 ::Assemble source files.
 echo assembling...
 cd %SRC_DIR%
-rgbasm -oremnant.obj remnant.asm
+rgbasm95 -i%EXTERNAL_INCLUDES% -i%INCLUDES% -oremnant.obj remnant.asm
 if errorlevel 1 goto end
 
 ::Link.
 echo linking...
 cmd /C ..\makelnk remnant > remnant.lnk
-xlink -mremnant.map remnant.lnk
+xlink95 -mremnant.map remnant.lnk
 if errorlevel 1 goto end
 
 ::Fix checksums.
 echo fixing...
-rgbfix -v remnant.gb
+rgbfix95 -v remnant.gb
 
 ::Copy to binary directory.
 copy /V "remnant.gb" %BUILD_DIR%
