a4676425caeb3f9b9a360b47f61480c91f2d5365
Megan Worley <megan.worley@live.com>
2016-03-08 20:57:46 -0800

    Updated build to use executables in the tools folder.  Set the tools to be the 95 versions of RGBASM (having trouble with includes with the newer versions).  Added a .bat file to run the game.

--- build.bat
+++ build.bat
@@ -3,33 +3,44 @@
 ::Create binary directory
 set SRC_DIR="%~dp0src"
 set BUILD_DIR="%~dp0bin"
-set INCLUDES=%~dp0include\
-set EXTERNAL_INCLUDES=%~dp0external\include\
+if not exist %BUILD_DIR% (
 mkdir %BUILD_DIR%
+echo Build directory created...
+)
+
+::Set paths for assembling the source files.
+set PROJECT_INCLUDES=%~dp0include\ 
+set EXTERNAL_INCLUDES=%~dp0external\include\
+set ASSEMBLER=%~dp0tools\RGBASM\rgbasm95.exe
+set LINKER=%~dp0tools\RGBASM\xlink95.exe
+set CHECKSUM_FIXER=%~dp0tools\RGBASM\rgbfix95.exe
 
 ::Assemble source files.
-echo assembling...
+echo Assembling...
 cd %SRC_DIR%
-rgbasm95 -i%EXTERNAL_INCLUDES% -i%INCLUDES% -oremnant.obj remnant.asm
+start /WAIT %ASSEMBLER% -i%EXTERNAL_INCLUDES% -i%PROJECT_INCLUDES -oremnant.obj remnant.asm
 if errorlevel 1 goto end
 
 ::Link.
-echo linking...
+echo Linking...
 cmd /C ..\makelnk remnant > remnant.lnk
-xlink95 -mremnant.map remnant.lnk
+start /WAIT %LINKER% -mremnant.map remnant.lnk
 if errorlevel 1 goto end
 
 ::Fix checksums.
-echo fixing...
-rgbfix95 -v remnant.gb
+echo Fixing...
+start /B /WAIT %CHECKSUM_FIXER% -v remnant.gb
 
 ::Copy to binary directory.
+echo Copying files to build directory...
 copy /V "remnant.gb" %BUILD_DIR%
 copy /V "remnant.map" %BUILD_DIR%
 
 :end
+echo Cleaning up...
 del *.obj
 del *.lnk
-del *.gb
 del *.map
+del *.gb
 cd ..\
+echo Build complete.
---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set ROM=bin/remnant.gb
+if not exist %ROM% (
+echo Game ROM not found!  Run build.bat first to build the game.
+) ELSE (
+start %EMULATOR% bin/remnant.gb
+)
